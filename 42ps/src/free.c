4180d80697e2c3cc7c2353e054cc55fc246ef6bf
adanfindo <92444945+adanfindo@users.noreply.github.com>
2022-02-28 11:29:13 +0100

    Add files via upload

---
+++ 42ps/src/free.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afindo <afindo@student.roma42.it>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/28 09:30:58 by afindo            #+#    #+#             */
+/*   Updated: 2022/02/28 09:31:00 by afindo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/push_swap.h"
+
+void		free_arg(char **argv)
+{
+	int i;
+
+	i = -1;
+	while (argv[++i])
+		free(argv[i]);
+	free(argv);
+}
+
+void		free_stack(t_stack *stack)
+{
+	t_elem	*actual;
+	t_elem	*next;
+
+	while (stack && stack->first)
+	{
+		actual = stack->first;
+		next = stack->first;
+		while (next->next)
+		{
+			actual = next;
+			next = next->next;
+		}
+		if (actual == next)
+		{
+			free(stack->first);
+			stack->first = NULL;
+		}
+		else
+		{
+			actual->next = NULL;
+			free(next);
+		}
+	}
+	free(stack);
+}
+
+void		ft_exit(t_mall *mall, int n)
+{
+	if (mall->is_mall == 1)
+		free_arg(mall->argv);
+	if (n >= 1)
+		free_stack(mall->stack_a);
+	if (n >= 2)
+		free_stack(mall->stack_b);
+	if (n <= 2)
+		ft_error("Error");
+	if (n > 2)
+		exit(EXIT_SUCCESS);
+}
---
+++ 42ps/src/src_pswap/print.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afindo <afindo@student.roma42.it>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/28 09:31:44 by afindo            #+#    #+#             */
+/*   Updated: 2022/02/28 09:31:45 by afindo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "../../inc/push_swap.h"
+
+void	do_print_ra(t_stack *stack_a)
+{
+	rotate(stack_a);
+	write(1, "ra\n", 3);
+}
+
+void	do_print_rra(t_stack *stack_a)
+{
+	reverse_rotate(stack_a);
+	write(1, "rra\n", 4);
+}
+
+void	do_print_rb(t_stack *stack_b)
+{
+	rotate(stack_b);
+	write(1, "rb\n", 3);
+}
+
+void	do_print_rrb(t_stack *stack_b)
+{
+	reverse_rotate(stack_b);
+	write(1, "rrb\n", 4);
+}
+
+void	do_print_sa(t_stack *stack_a)
+{
+	swap(stack_a);
+	write(1, "sa\n", 3);
+}
